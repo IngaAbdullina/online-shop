f94bccdb67c416b0619614b18aa128a6886ba3c7
abdullina <abdullina@speechpro.com>
2023-03-29 16:49:06 +0300

    Configuration changes

--- src/main/java/com/itmo/online/shop/config/SecurityConfiguration.java
+++ src/main/java/com/itmo/online/shop/config/SecurityConfiguration.java
@@ -1,29 +1,26 @@
 package com.itmo.online.shop.config;
 
-import com.itmo.online.shop.config.authentication.ShopAuthenticationManager;
 import com.itmo.online.shop.service.impl.UserSecurityService;
 import com.itmo.online.shop.util.SecurityUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
-  @Autowired
-  private UserSecurityService userSecurityService;
+  private final UserSecurityService userSecurityService;
 
-  private BCryptPasswordEncoder passwordEncoder() {
-    return SecurityUtility.passwordEncoder();
+  public SecurityConfiguration(UserSecurityService userSecurityService) {
+    this.userSecurityService = userSecurityService;
   }
 
   private static final String[] PUBLIC_MATCHERS = {
@@ -37,27 +34,37 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
       "/article-detail"
   };
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
+        .cors().disable()
+        .csrf().disable()
         .authorizeRequests()
         .antMatchers(PUBLIC_MATCHERS).permitAll()
         .antMatchers("/article/**").hasRole("ADMIN")
-        .anyRequest().authenticated();
-
-    http
-        .csrf().disable().cors().disable()
-        .formLogin().failureUrl("/login?error")
+        .anyRequest().authenticated()
+        .and()
+        .formLogin()
         .loginPage("/login").permitAll()
+        .failureUrl("/login?error")
         .and()
         .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
         .logoutSuccessUrl("/?logout").deleteCookies("remember-me").permitAll()
         .and()
         .rememberMe().key("aSecretKey");
+    return http.build();
   }
 
-  @Autowired
-  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(userSecurityService).passwordEncoder(passwordEncoder());
+  @Bean
+  public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+    return http.getSharedObject(AuthenticationManagerBuilder.class)
+        .userDetailsService(userSecurityService)
+        .passwordEncoder(passwordEncoder())
+        .and()
+        .build();
+  }
+
+  private BCryptPasswordEncoder passwordEncoder() {
+    return SecurityUtility.passwordEncoder();
   }
 }
